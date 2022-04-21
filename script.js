3aec478e76d32b7e2ec56e15be5e0fbebda6e93d
QodeMaster <59826211+QodeMaster@users.noreply.github.com>
2022-04-21 13:54:53 +0200

    Added drawChecker functionality

--- script.js
+++ script.js
@@ -5,24 +5,36 @@ const ctx = c0.getContext("2d");
 const width  = c0.width;
 const height = c0.height;
 
-const sideLengthOfSquare = width / 8;
-
-for(let i = 0; i < height/sideLengthOfSquare; i++) {
-  for(let j = 0; j < width/sideLengthOfSquare; j++) {
-    ctx.fillStyle = ((i + j) % 2 == 0 ? "Moccasin" : "Tan");
-    ctx.fillRect(i * sideLengthOfSquare, j * sideLengthOfSquare, sideLengthOfSquare, sideLengthOfSquare);
-  }
-}
+const sideLengthOfSquare        = width / 8;  // 50px
+const halfWaySideLengthOfSquare = width / 16; // 25px
+const checkerRadius             = 20;
 
 function drawBoard() {
-	// Will draw board
+	for(let i = 0; i < height/sideLengthOfSquare; i++) {
+    for(let j = 0; j < width/sideLengthOfSquare; j++) {
+      ctx.fillStyle = ((i + j) % 2 == 0 ? "Moccasin" : "Tan");
+      ctx.fillRect(i * sideLengthOfSquare, j * sideLengthOfSquare, sideLengthOfSquare, sideLengthOfSquare);
+    }
+  }
+}
+drawBoard();
+
+function drawChecker(checkPiece) {
+  ctx.fillStyle = (checkPiece.colorBoolean ? "Red" : "Black");
+  ctx.beginPath();
+  ctx.arc(halfWaySideLengthOfSquare + checkPiece.i * sideLengthOfSquare,
+          halfWaySideLengthOfSquare + checkPiece.j * sideLengthOfSquare,
+          checkerRadius,
+          0,
+          Math.PI*2);
+  ctx.fill(); // No need for closePath();
 }
 
 class Checker {
-  constructor(color, i, j) {
-  	this.color = color;
+  constructor(i, j, colorBoolean) {
     this.i = i;
     this.j = j;
+    this.colorBoolean = colorBoolean;
     this.isKing = false;
   }
 }
